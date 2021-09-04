4de1111b086bbb8fb6a0a2e5fae5aea154c41f67
yashsehgal <yashsehgal.work@gmail.com>
2021-09-05 02:12:44 +0530

    feat: icon widget-component added

---
+++ src/styled-components/widgets/Icon/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Icon extends React.Component {
+  render() {
+    if (this.props.size === 'default' || this.props.size === undefined) {
+      return (
+        <i className={`${this.props.name}`} style={{ fontSize: '12px' }}></i>
+      )
+    } else if (this.props.size === 'medium') {
+      return (
+        <i className={`${this.props.name}`} style={{ fontSize: '16px' }}></i>
+      )
+    } else if (this.props.size === 'large') {
+      return (
+        <i className={`${this.props.name}`} style={{ fontSize: '20px' }}></i>
+      )
+    }
+  }
+}
