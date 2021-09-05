51d2453d9faedd588bf7bf0558139d049a793f98
yashsehgal <yashsehgal.work@gmail.com>
2021-09-05 21:04:41 +0530

    feat: dashboard-header section added

---
+++ src/views/Dashboard/Board/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.view.dashboard.board.css';
+
+export default class Board extends React.Component {
+  render() {
+    return (
+      <div className="board">
+        
+      </div>
+    )
+  }
+}
--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Menu from '../../styled-components/section-components/Menu';
+import { OutlineButton, PrimaryButton } from '../../styled-components/widgets/Button';
 import './style.views.dashboard.css'
 
 export default function Dashboard() {
@@ -9,7 +10,19 @@ export default function Dashboard() {
       <Menu />
       {/* Board/Cards Section */}
       <div className="board-wrapper">
-        
+        <div className="board-header-section">
+          <h5 className="dashboard-view-title text__gray-1">Dashboard / All Tasks</h5>
+          <div className="dashboard-task-action-buttons-wrapper">
+            <PrimaryButton
+              value="New Task"
+              icon="fas fa-plus"
+            />
+            <OutlineButton 
+              value="Edit Columns"
+              icon="fas fa-pen"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
