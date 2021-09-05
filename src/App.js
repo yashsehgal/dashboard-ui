b0a888a16faad1a6c8a7d49ce393c33c8c1acdd1
yashsehgal <yashsehgal.work@gmail.com>
2021-09-06 00:39:11 +0530

    update: app-routes modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter, Route } from "react-router-dom";
 import "./styled-components/index.css";
 import Sidebar from "./styled-components/section-components/Sidebar";
 import Dashboard from "./views/Dashboard";
+import Profile from "./views/Profile";
 import Projects from "./views/Projects";
+import Teams from "./views/Teams";
 
 export default class App extends React.Component {
   render() {
@@ -20,7 +22,12 @@ export default class App extends React.Component {
             <Route path="/" exact component={Dashboard} />
             <Route path="/home" exact component={Dashboard} />
             <Route path="/dashboard" exact component={Dashboard} />
+            {/* route for projects view */}
             <Route path="/projects" exact component={Projects} />
+            {/* route for teams view */}
+            <Route path="/teams" exact component={Teams} />
+            {/* route for profile view */}
+            <Route path="/profile" exact component={Profile} />
           </BrowserRouter>
         </div>
       </div>
