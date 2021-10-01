062a05968bb7454877076683d25d01e74191cbcb
yashsehgal <yashsehgal.work@gmail.com>
2021-10-02 03:07:18 +0530

    feat: toast feature initial structure added

--- src/styled-components/widgets/Toast/index.js
+++ src/styled-components/widgets/Toast/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.widgets.toast.css";
+import ReactModal from "react-modal";
 
 export default function Toast() {
-  return <div className="toast-wrapper" />;
+  
 }
