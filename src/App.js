92729558f9798ee7e4dd50d952afbef0fc9d07c7
Yash Sehgal <yashsehgal.work@gmail.com>
2021-10-14 21:18:52 +0000

    ffeat: 404-page-not-found view added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./styled-components/index.css";
 import Sidebar from "./styled-components/section-components/Sidebar";
 import Chats from "./views/Chats";
@@ -8,6 +8,7 @@ import Profile from "./views/Profile";
 import Projects from "./views/Projects";
 import Teams from "./views/Teams";
 import "animate.css";
+import PageNotFound from "./views/PageNotFound";
 
 export default class App extends React.Component {
   render() {
@@ -23,18 +24,21 @@ export default class App extends React.Component {
           <div className="app-wrapper">
             {/* setting up routers using hash-router */}
             <BrowserRouter>
-              {/* routes for home-view / dashboard-view */}
-              <Route path="/" exact component={Dashboard} />
-              <Route path="/home" exact component={Dashboard} />
-              <Route path="/dashboard" exact component={Dashboard} />
-              {/* route for projects view */}
-              <Route path="/projects" exact component={Projects} />
-              {/* route for teams view */}
-              <Route path="/teams" exact component={Teams} />
-              {/* route for profile view */}
-              <Route path="/profile" exact component={Profile} />
-              {/* route for chat view */}
-              <Route path="/chats" exact component={Chats} />
+              <Switch>
+                {/* routes for home-view / dashboard-view */}
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/home" exact component={Dashboard} />
+                <Route path="/dashboard" exact component={Dashboard} />
+                {/* route for projects view */}
+                <Route path="/projects" exact component={Projects} />
+                {/* route for teams view */}
+                <Route path="/teams" exact component={Teams} />
+                {/* route for profile view */}
+                <Route path="/profile" exact component={Profile} />
+                {/* route for chat view */}
+                <Route path="/chats" exact component={Chats} />
+                <Route component={PageNotFound} />
+              </Switch>
             </BrowserRouter>
           </div>
         </div>
