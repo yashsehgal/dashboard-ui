9ee324a646f5d7b9352af8e4be57788f38bfba96
Yash Sehgal <yashsehgal.work@gmail.com>
2021-10-14 19:43:03 +0000

    feat: state property added for create-new-project modal

--- src/styled-components/widgets/Modal/CreateNewProjectModal/index.js
+++ src/styled-components/widgets/Modal/CreateNewProjectModal/index.js
@@ -1,12 +1,20 @@
 import './style.widget.modal.create-new-project-modal.css';
 import React from 'react';
+import Input from '../../Input';
 
 export default function CreateNewProjectModal() {
     return (
         <div className="create-new-project-modal">
             <h4 className="create-new-project-modal-title">Add a new project</h4>
             <div className="create-new-project-details-form-wrapper">
-                
+                <div className="form-input-wrapper" id="project-title-wrapper">
+                    <h6 className="label-project-title">Project title</h6>
+                    <Input type="text" 
+                        placeholder="Add the new project name"
+                        icon="fas fa-project-diagram"
+                        size="medium"
+                    />
+                </div>
             </div>
         </div>
     )
--- src/views/Projects/index.js
+++ src/views/Projects/index.js
@@ -3,12 +3,16 @@ import ProjectCard from "../../styled-components/widgets/Card/ProjectCard";
 import "./style.views.projects.css";
 import Input from "../../styled-components/widgets/Input";
 import { PrimaryButton } from "../../styled-components/widgets/Button";
+import ReactModal from "react-modal";
 
 // importing sample-data for testing frontend
 import ProjectData from "./__projects.json";
+import CreateNewProjectModal from "../../styled-components/widgets/Modal/CreateNewProjectModal";
 
 export default function Projects() {
   const [_projectData] = useState(ProjectData);
+  const [createNewProjectModalPopupState, setCreateNewProjectModalPopupState] = useState(false);
+  
   console.log(_projectData);
   return (
     <div className="view view__projects">
@@ -20,7 +24,7 @@ export default function Projects() {
           icon="fas fa-search"
         />
         <PrimaryButton value="Filter Projects" icon="fas fa-filter" />
-        <div className="projects-action-button-wrapper">
+        <div className="projects-action-button-wrapper" onClick={() => setCreateNewProjectModalPopupState(true)}>
           <PrimaryButton value="Add a new Project" icon="fas fa-plus" />
         </div>
       </div>
@@ -36,6 +40,37 @@ export default function Projects() {
           />
         ))}
       </div>
+      <ReactModal
+        isOpen={createNewProjectModalPopupState}
+        onRequestClose={() => setCreateNewProjectModalPopupState(false)}
+        style={{
+          overlay: {
+            backgroundColor: "#00000025",
+          },
+          content: {
+            width: "fit-content",
+            height: "fit-content",
+            padding: "3em",
+            margin: "auto",
+            boxShadow: "0px 6px 20px #00000045",
+          },
+        }}
+      >
+        <CreateNewProjectModal />
+
+        {/* action-buttons wrapper */}
+        <div
+          className="modal-action-button-wrapper"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <div
+            className="button-wrap-fit-content"
+            
+          >
+            <PrimaryButton value="Create new project" />
+          </div>
+        </div>
+      </ReactModal>
     </div>
   );
 }
