a6e41494b251073ac83752488bac6a143e983392
Yash Sehgal <yashsehgal.work@gmail.com>
2021-10-14 21:05:41 +0000

    feat: funny text to handle un-responsiveness added

--- src/App.js
+++ src/App.js
@@ -13,25 +13,30 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="view__app">
-        {/* sidebar */}
-        <Sidebar />
-
-        <div className="app-wrapper">
-          {/* setting up routers using hash-router */}
-          <BrowserRouter>
-            {/* routes for home-view / dashboard-view */}
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/home" exact component={Dashboard} />
-            <Route path="/dashboard" exact component={Dashboard} />
-            {/* route for projects view */}
-            <Route path="/projects" exact component={Projects} />
-            {/* route for teams view */}
-            <Route path="/teams" exact component={Teams} />
-            {/* route for profile view */}
-            <Route path="/profile" exact component={Profile} />
-            {/* route for chat view */}
-            <Route path="/chats" exact component={Chats} />
-          </BrowserRouter>
+        <div className="funny-alert-view-msg">
+          <h1>Your expectations are reaching sky-heights :)</h1>
+          <p>Try switching to landscape/desktop mode for proper usage</p>
+        </div>
+        <div className="view__app-container">
+          {/* sidebar */}
+          <Sidebar />
+          <div className="app-wrapper">
+            {/* setting up routers using hash-router */}
+            <BrowserRouter>
+              {/* routes for home-view / dashboard-view */}
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/home" exact component={Dashboard} />
+              <Route path="/dashboard" exact component={Dashboard} />
+              {/* route for projects view */}
+              <Route path="/projects" exact component={Projects} />
+              {/* route for teams view */}
+              <Route path="/teams" exact component={Teams} />
+              {/* route for profile view */}
+              <Route path="/profile" exact component={Profile} />
+              {/* route for chat view */}
+              <Route path="/chats" exact component={Chats} />
+            </BrowserRouter>
+          </div>
         </div>
       </div>
     );
