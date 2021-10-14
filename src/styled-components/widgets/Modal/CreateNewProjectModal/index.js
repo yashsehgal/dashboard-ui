a731809ce7fa9e9c9fd59c0a585eaaa14ecfc20a
Yash Sehgal <yashsehgal.work@gmail.com>
2021-10-14 19:30:24 +0000

    feat: create-new-project modal added

---
+++ src/styled-components/widgets/Modal/CreateNewProjectModal/index.js
@@ -0,0 +1,13 @@
+import './style.widget.modal.create-new-project-modal.css';
+import React from 'react';
+
+export default function CreateNewProjectModal() {
+    return (
+        <div className="create-new-project-modal">
+            <h4 className="create-new-project-modal-title">Add a new project</h4>
+            <div className="create-new-project-details-form-wrapper">
+                
+            </div>
+        </div>
+    )
+}
