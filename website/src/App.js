0d2a78bd142d92828818353a966fb1f0152bc233
Yash Sehgal <yashsehgal.work@gmail.com>
2021-10-15 20:53:20 +0000

    feat: (wip) home-view created

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './styles/style.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './views/Home';
 
 export default function App () {
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ website/src/views/Home/index.js
@@ -0,0 +1,10 @@
+import './style.view.home.css';
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div className="home">
+            This is the home view rendering
+        </div>
+    )
+}
