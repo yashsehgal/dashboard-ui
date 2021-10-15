d726aade2810cfa0d900d06c4d7f34186cfd27b5
Yash Sehgal <yashsehgal.work@gmail.com>
2021-10-15 20:48:36 +0000

    update(wip): react-app dependencies added

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import './styles/style.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default function App () {
-  return <div className='app' />
+  return (
+    <div className="app">
+
+    </div>
+  )
 }
